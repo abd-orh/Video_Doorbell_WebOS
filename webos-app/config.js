a1c42965d0752b98ea690226bdc323033cc3ebba
abd-orh <abdula.orhan@gmail.com>
2026-07-29 15:03:22 +0300

    Update config.js

--- webos-app/config.js
+++ webos-app/config.js
@@ -1,7 +1,7 @@
 /* Video Doorbell for LG webOS */
 const CONFIG = {
     // /config/www/reolink/ is exposed by Home Assistant as /local/reolink/
-    BASE_URL: "http://192.168.1.78:8123/local/reolink/",
+    BASE_URL: "http://XXX.XXX.XXX.XXX:8123/local/reolink/",
     // Delay between displayed snapshots, in milliseconds
     REFRESH_INTERVAL: 250,
     // Overlay visibility time, in milliseconds
